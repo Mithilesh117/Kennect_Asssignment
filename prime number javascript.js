31b432791a047619950337fe7357f5302ed36206
Mithilesh117 <96907690+Mithilesh117@users.noreply.github.com>
2022-11-27 10:01:20 +0530

    Add files via upload

---
+++ prime number javascript.js
@@ -0,0 +1,15 @@
+const number_1 = 7;
+const number_2 = 11;
+const diff = number_1-number_2
+// var number_2 = 11;
+for (let i=2;i<=number_1;i++)
+    if (number_1 % i  === 0)
+    {
+        console.log('$ not prime ');
+    break;
+    }else{
+        console.log(' prime')
+        console.log("the difference between number_1 and number_2 is  ",diff)
+        break;
+        
+    }
